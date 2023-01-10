056a47aa3f04d498d65f6ee82d70de8f2797a0a9
Chris Leveille <cdleveille@gmail.com>
2023-01-09 22:20:03 -0500

    misc updates

--- src/server/services/db.ts
+++ src/server/services/db.ts
@@ -4,6 +4,7 @@ import { Config } from "../helpers/config";
 
 export const connectToDatabase = async () => {
 	try {
+		console.log(`Connecting to database on ${Config.MONGO_URI}...`);
 		await mongoose.connect(Config.MONGO_URI);
 		console.log(`Connected to database: ${Config.MONGO_URI}`);
 	} catch (error) {
