4e75fc3bbed2b23d40c6f3c7338386b488d058bd
Chris Leveille <cdleveille@gmail.com>
2023-09-05 00:15:22 -0400

    upgrade packages & cleanup

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,19 +1,13 @@
 /** https://webpack.js.org/configuration/ */
 
-import fs from "fs";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from "path";
 import { Configuration } from "webpack";
 import WebpackObfuscator from "webpack-obfuscator";
 import { InjectManifest } from "workbox-webpack-plugin";
 
+import project from "./package.json";
 import { Config } from "./src/server/helpers/config";
-
-const packageJson = JSON.parse(fs.readFileSync("package.json").toString()) as {
-	name: string;
-	description: string;
-	author: string;
-};
 
 export default {
 	mode: Config.IS_PROD ? "production" : "development",
@@ -88,9 +82,9 @@ export default {
 		}),
 
 		new HtmlWebpackPlugin({
-			title: packageJson.name,
-			description: packageJson.description,
-			author: packageJson.author,
+			title: project.name,
+			description: project.description,
+			author: project.author,
 			filename: "index.html",
 			template: path.resolve(__dirname, "./src/client/_index.html")
 		}),
